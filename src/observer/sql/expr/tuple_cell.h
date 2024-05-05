d31945fc33f374400835fe17c80a54eb14c9412e
cxhagd <1460873268@qq.com>
2024-05-05 21:10:45 +0800

    第一次提交该文件

---
+++ src/observer/sql/expr/tuple_cell.h
@@ -0,0 +1,36 @@
+/* Copyright (c) 2021 OceanBase and/or its affiliates. All rights reserved.
+miniob is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details. */
+
+//
+// Created by WangYunlai on 2022/6/7.
+//
+
+#pragma once
+
+#include "storage/field/field_meta.h"
+#include <iostream>
+
+class TupleCellSpec
+{
+public:
+  TupleCellSpec(const char *table_name, const char *field_name, const char *alias = nullptr);
+  TupleCellSpec(const char *alias);
+  TupleCellSpec(const char *table_name, const char * field_name, const char *alias, const AggrOp aggr);
+  TupleCellSpec(const char *alias, const AggrOp aggr);
+  const char *table_name() const { return table_name_.c_str(); }
+  const char *field_name() const { return field_name_.c_str(); }
+  const char *alias() const { return alias_.c_str(); }
+  void aggr_to_string (const AggrOp aggr,std::string& aggr_expr);
+private:
+  std::string table_name_;
+  std::string field_name_;
+  AggrOp aggr_;
+  std::string alias_;
+};
---
+++ src/observer/sql/parser/value.cpp
@@ -0,0 +1,420 @@
+/* Copyright (c) 2021 OceanBase and/or its affiliates. All rights reserved.
+miniob is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details. */
+
+//
+// Created by WangYunlai on 2023/06/28.
+//
+
+#include "sql/parser/value.h"
+#include "common/lang/comparator.h"
+#include "common/lang/string.h"
+#include "common/log/log.h"
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+
+const char *ATTR_TYPE_NAME[] = {"undefined", "chars", "ints", "dates", "floats", "booleans"};
+
+const char *attr_type_to_string(AttrType type)
+{
+  if (type >= UNDEFINED && type <= FLOATS) {
+    return ATTR_TYPE_NAME[type];
+  }
+  return "unknown";
+}
+AttrType attr_type_from_string(const char *s)
+{
+  for (unsigned int i = 0; i < sizeof(ATTR_TYPE_NAME) / sizeof(ATTR_TYPE_NAME[0]); i++) {
+    if (0 == strcmp(ATTR_TYPE_NAME[i], s)) {
+      return (AttrType)i;
+    }
+  }
+  return UNDEFINED;
+}
+
+Value::Value(int val) { set_int(val); }
+
+Value::Value(float val) { set_float(val); }
+
+Value::Value(bool val) { set_boolean(val); }
+
+Value::Value(const char *s, int len /*= 0*/) { set_string(s, len); }
+
+//***********************date类构造函数*******************************
+Value::Value(const char *s, int len , int flag){
+  std::istringstream iss(s);
+  int year,month,day;
+  char dash;
+  iss>>year>>dash>>month>>dash>>day;
+  if(!isValidDate(year,month,day)){
+    throw std::string("invalid date");
+    return;
+  }
+  int val=date_to_days(year,month,day);
+  set_date(val);
+}
+//*******************************************************************
+
+void Value::set_data(char *data, int length)
+{
+  switch (attr_type_) {
+    case CHARS: {
+      set_string(data, length);
+    } break;
+    case INTS: {
+      num_value_.int_value_ = *(int *)data;
+      length_               = length;
+    } break;
+    case DATES: {
+      num_value_.date_value_ = *(int *)data;
+      length_               = length;
+    } break;
+    case FLOATS: {
+      num_value_.float_value_ = *(float *)data;
+      length_                 = length;
+    } break;
+    case BOOLEANS: {
+      num_value_.bool_value_ = *(int *)data != 0;
+      length_                = length;
+    } break;
+    default: {
+      LOG_WARN("unknown data type: %d", attr_type_);
+    } break;
+  }
+}
+void Value::set_int(int val)
+{
+  attr_type_            = INTS;
+  num_value_.int_value_ = val;
+  length_               = sizeof(val);
+}
+
+//*********************set_date**************************
+void Value::set_date(int val)
+{
+  attr_type_            = DATES;
+  num_value_.date_value_ = val;
+  length_               = sizeof(val);
+}
+//*******************************************************
+
+void Value::set_float(float val)
+{
+  attr_type_              = FLOATS;
+  num_value_.float_value_ = val;
+  length_                 = sizeof(val);
+}
+void Value::set_boolean(bool val)
+{
+  attr_type_             = BOOLEANS;
+  num_value_.bool_value_ = val;
+  length_                = sizeof(val);
+}
+void Value::set_string(const char *s, int len /*= 0*/)
+{
+  attr_type_ = CHARS;
+  if (len > 0) {
+    len = strnlen(s, len);
+    str_value_.assign(s, len);
+  } else {
+    str_value_.assign(s);
+  }
+  length_ = str_value_.length();
+}
+
+void Value::set_value(const Value &value)
+{
+  switch (value.attr_type_) {
+    case INTS: {
+      set_int(value.get_int());
+    } break;
+    case DATES: {
+      set_date(value.get_date());
+    } break;
+    case FLOATS: {
+      set_float(value.get_float());
+    } break;
+    case CHARS: {
+      set_string(value.get_string().c_str());
+    } break;
+    case BOOLEANS: {
+      set_boolean(value.get_boolean());
+    } break;
+    case UNDEFINED: {
+      ASSERT(false, "got an invalid value type");
+    } break;
+  }
+}
+
+const char *Value::data() const
+{
+  switch (attr_type_) {
+    case CHARS: {
+      return str_value_.c_str();
+    } break;
+    default: {
+      return (const char *)&num_value_;
+    } break;
+  }
+}
+
+std::string Value::to_string() const
+{
+  std::stringstream os;
+  switch (attr_type_) {
+    case INTS: {
+      os << num_value_.int_value_;
+    } break;
+     case DATES: {
+      std::string str=days_to_datestr(num_value_.date_value_);
+      os << str;
+    } break;
+    case FLOATS: {
+      os << common::double_to_str(num_value_.float_value_);
+    } break;
+    case BOOLEANS: {
+      os << num_value_.bool_value_;
+    } break;
+    case CHARS: {
+      os << str_value_;
+    } break;
+    default: {
+      LOG_WARN("unsupported attr type: %d", attr_type_);
+    } break;
+  }
+  return os.str();
+}
+
+int Value::compare(const Value &other) const
+{
+  if (this->attr_type_ == other.attr_type_) {
+    switch (this->attr_type_) {
+      case INTS: {
+        return common::compare_int((void *)&this->num_value_.int_value_, (void *)&other.num_value_.int_value_);
+      } break;
+      case DATES: {
+        return common::compare_date((void *)&this->num_value_.int_value_, (void *)&other.num_value_.int_value_);
+      } break;
+      case FLOATS: {
+        return common::compare_float((void *)&this->num_value_.float_value_, (void *)&other.num_value_.float_value_);
+      } break;
+      case CHARS: {
+        return common::compare_string((void *)this->str_value_.c_str(),
+            this->str_value_.length(),
+            (void *)other.str_value_.c_str(),
+            other.str_value_.length());
+      } break;
+      case BOOLEANS: {
+        return common::compare_int((void *)&this->num_value_.bool_value_, (void *)&other.num_value_.bool_value_);
+      }
+      default: {
+        LOG_WARN("unsupported type: %d", this->attr_type_);
+      }
+    }
+  } else if (this->attr_type_ == INTS && other.attr_type_ == FLOATS) {
+    float this_data = this->num_value_.int_value_;
+    return common::compare_float((void *)&this_data, (void *)&other.num_value_.float_value_);
+  } else if (this->attr_type_ == FLOATS && other.attr_type_ == INTS) {
+    float other_data = other.num_value_.int_value_;
+    return common::compare_float((void *)&this->num_value_.float_value_, (void *)&other_data);
+  }
+  LOG_WARN("not supported");
+  return -1;  // TODO return rc?
+}
+
+int Value::get_int() const
+{
+  switch (attr_type_) {
+    case CHARS: {
+      try {
+        return (int)(std::stol(str_value_));
+      } catch (std::exception const &ex) {
+        LOG_TRACE("failed to convert string to number. s=%s, ex=%s", str_value_.c_str(), ex.what());
+        return 0;
+      }
+    }
+    case INTS: {
+      return num_value_.int_value_;
+    }
+    case DATES: {
+      return num_value_.date_value_;
+    }
+    case FLOATS: {
+      return (int)(num_value_.float_value_);
+    }
+    case BOOLEANS: {
+      return (int)(num_value_.bool_value_);
+    }
+    default: {
+      LOG_WARN("unknown data type. type=%d", attr_type_);
+      return 0;
+    }
+  }
+  return 0;
+}
+
+//**********************get_date******************************
+int Value::get_date() const{
+  return get_int();
+}
+//************************************************************
+
+float Value::get_float() const
+{
+  switch (attr_type_) {
+    case CHARS: {
+      try {
+        return std::stof(str_value_);
+      } catch (std::exception const &ex) {
+        LOG_TRACE("failed to convert string to float. s=%s, ex=%s", str_value_.c_str(), ex.what());
+        return 0.0;
+      }
+    } break;
+    case INTS: {
+      return float(num_value_.int_value_);
+    } break;
+    case DATES: {
+      return float(num_value_.date_value_);
+    } break;
+    case FLOATS: {
+      return num_value_.float_value_;
+    } break;
+    case BOOLEANS: {
+      return float(num_value_.bool_value_);
+    } break;
+    default: {
+      LOG_WARN("unknown data type. type=%d", attr_type_);
+      return 0;
+    }
+  }
+  return 0;
+}
+
+std::string Value::get_string() const { return this->to_string(); }
+
+bool Value::get_boolean() const
+{
+  switch (attr_type_) {
+    case CHARS: {
+      try {
+        float val = std::stof(str_value_);
+        if (val >= EPSILON || val <= -EPSILON) {
+          return true;
+        }
+
+        int int_val = std::stol(str_value_);
+        if (int_val != 0) {
+          return true;
+        }
+
+        return !str_value_.empty();
+      } catch (std::exception const &ex) {
+        LOG_TRACE("failed to convert string to float or integer. s=%s, ex=%s", str_value_.c_str(), ex.what());
+        return !str_value_.empty();
+      }
+    } break;
+    case INTS: {
+      return num_value_.int_value_ != 0;
+    } break;
+    case DATES: {
+      return num_value_.date_value_ != 0;
+    } break;
+    case FLOATS: {
+      float val = num_value_.float_value_;
+      return val >= EPSILON || val <= -EPSILON;
+    } break;
+    case BOOLEANS: {
+      return num_value_.bool_value_;
+    } break;
+    default: {
+      LOG_WARN("unknown data type. type=%d", attr_type_);
+      return false;
+    }
+  }
+  return false;
+}
+
+
+//**********************************************************
+//-------------------------辅助函数--------------------------
+
+//-------------------------date_to_days---------------------
+int Value::date_to_days(int year,int month,int day)
+  {
+   int days = 0;
+    int y = 0;
+    // 计算年份之间的天数差
+    for (;y < year; ++y) {
+        days += is_leap_year(y) ? 366 : 365;
+    }
+    // 计算给定年份中月份之前的天数
+    for (int m = 1; m < month; ++m) {
+        days += days_in_month(year, m);
+    }
+    // 加上给定月份的天数
+    days += day - 1;
+    return days;
+  }
+
+int Value::is_leap_year(int year)const
+{
+    return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+}
+
+int Value::days_in_month(int year, int month)const
+{
+    int days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (month == 2 && is_leap_year(year)) {
+        return 29;
+    }
+    return days[month - 1];
+}
+
+std::string Value::days_to_datestr(int val)const
+{
+  int year = 0;
+  while (val >= 365) {
+    val -= (is_leap_year(year) ? 366 : 365);
+    year++;
+  }
+  int month = 1;
+  while (val >= days_in_month(year, month)) {
+      val -= days_in_month(year, month);
+      month++;
+  }
+    std::ostringstream oss;
+    oss << std::setw(4)<< std::setfill('0')<<year << "-" 
+        << std::setw(2) << std::setfill('0') << month <<"-"
+        << std::setw(2) << std::setfill('0') << val + 1;
+    return oss.str();
+}
+
+bool Value::isValidDate(int year,int month,int day)
+{
+    // 检查月份范围
+    if (month < 1 || month > 12) return false;  
+    // 检查日期范围
+    if (day < 1 || day > 31) return false; 
+    // 检查2月份的天数（闰年）
+    if (month == 2) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return day <= 29;
+        } else {
+            return day <= 28;
+        }
+    }
+    // 检查30天的月份
+    if (month == 4 || month == 6 || month == 9 || month == 11) {
+        return day <= 30;
+    }
+    return true;
+}
+//----------------------------------------------------------
+//**********************************************************
